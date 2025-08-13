9b0fd62261d69e97a313ce2b112020fafca8a9a7
kylenoad <email@kylenoad.co.uk>
2025-08-13 14:06:55 +0100

    Fix Supabase database connection for production

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,8 +2,9 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("FLASK_ENV") != "production":
+    load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def connect_to_db():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(DATABASE_URL, sslmode='require')
